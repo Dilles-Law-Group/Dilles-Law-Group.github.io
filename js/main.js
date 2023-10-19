e250fb30c5c0e330431d4a7b468351c5edd6ea2e
Michael Georgariou <mgeorgar@amazon.com>
2023-10-19 14:23:24 -0700

    Add team images, insert Nick and Samuel on homepage

--- js/main.js
+++ js/main.js
@@ -91,10 +91,32 @@
                 items:2
             },
             768:{
-                items:3
+                items:2
             },
             992:{
-                items:4
+                items:3
+            }
+        }
+    });
+
+    // Attorney carousel
+    $(".attorney-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: false,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
             }
         }
     });
